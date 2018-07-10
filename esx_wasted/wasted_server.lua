7836d4567c27d628267122174412230a3f3babf5
MoravianLion <38085706+MoravianLion@users.noreply.github.com>
2018-07-10 19:57:50 +0100

    Add files via upload

--- esx_wasted/wasted_server.lua
+++ esx_wasted/wasted_server.lua
@@ -8,7 +8,9 @@ AddEventHandler('esx_wastedscreen:moneyloss', function(bank)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	local _source = source
 	local BankAccount = xPlayer.getAccount('bank').money
+	local DirtyMoney = xPlayer.getAccount('black_money').money
 	xPlayer.removeAccountMoney('bank', BankAccount)
+	xPlayer.removeAccountMoney('black_money', DirtyMoney)
 	xPlayer.setMoney(500)
 	
 	TriggerClientEvent('esx:showNotification', xPlayer.source, ('Uff, fortunately, your fortune managed to cover the cost of national health care!'))
